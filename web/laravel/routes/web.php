28143cd88cc176f066ffb500b53dd03d5f3c1132
YutaSakurai2023 <mogle1989@gmail.com>
2023-07-13 13:00:01 +0900

    vuesplashツールテンプレート作成

--- web/laravel/routes/web.php
+++ web/laravel/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
+Route::get('/{any?}', fn() => view('index'))->where('any', '.+');
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +12,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
